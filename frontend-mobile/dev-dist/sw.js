45a689680f1738a180816f985596b30d97c49a3f
unknown <arnavchikhale30@gmail.com>
2026-08-06 12:16:19 +0530

    sidebar , bookings , attendance changed

--- frontend-mobile/dev-dist/sw.js
+++ frontend-mobile/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-a24bf94b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.svuvngi7do8"
+    "revision": "0.2asr88fgdh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
